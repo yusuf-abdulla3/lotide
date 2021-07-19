8b0aedf1146841698c2165d37193db1bbc900136
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-19 23:35:19 +0000

    Compares expected and actual values and tests whether they're equal or not

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Test code
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("five",1);
