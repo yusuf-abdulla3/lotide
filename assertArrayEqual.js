5f5151d3f2a0493d464aa2f1e17ed867c96d411b
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-21 04:33:19 +0000

    combined 2 different functions to make this function that is able to compare to different arrays.

---
+++ assertArrayEqual.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function(array1, array2) {
+  assertEqual(array1, array2);
+};
+
+const assertEqual = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+  for (let i = 0; i < array1.length; i++) {
+    //console.log(array1[i], array2[i]);
+    //console.log("element: " + arrayX, arrayY + "type: " + typeof(arrayX), typeof(arrayY));
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    }  
+  }
+  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]))
