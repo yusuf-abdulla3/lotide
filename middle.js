b1aaec0d3ee1f573ed6dec2ed8e9af6048d55bda
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-21 19:05:27 +0000

    Linted the code

--- middle.js
+++ middle.js
@@ -12,7 +12,7 @@ const assertEqual = function(array1, array2) {
     //console.log("element: " + arrayX, arrayY + "type: " + typeof(arrayX), typeof(arrayY));
     if (array1[i] !== array2[i]) {
       console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    }  
+    }
   }
   console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
 };
@@ -24,7 +24,7 @@ const middle = (num) => {
     if (num.length <= 2) {
       //console.log(numArray);
     } else if (num.length % 2 === 0) {
-      numArray[0] = num[(num.length / 2) -1];
+      numArray[0] = num[(num.length / 2) - 1];
       numArray[1] = num[num.length / 2];
     } else if (num.length % 2 !== 0) {
       numArray[0] = num[Math.floor(num.length / 2)];
@@ -34,7 +34,7 @@ const middle = (num) => {
   return numArray;
 };
 
-//TEST CODE 
+//TEST CODE
 middle([1, 2, 3, 4]); // => [2, 3]
 middle([1, 2, 3, 4, 5, 6]);
 
