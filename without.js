41db2c87a2613576d88ceb45be5c28921076dd6b
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-21 12:41:41 +0000

    Used eslint and checked the code lint and made sure it was up to standard

--- without.js
+++ without.js
@@ -2,12 +2,9 @@
 //Side Effect Function
 const without = function(source, itemsToRemove) {
   console.log('Elements: ' + source, itemsToRemove);
-  myArray = source.filter( ( el ) => !itemsToRemove.includes( el ) );
+  const myArray = source.filter((el) => !itemsToRemove.includes(el));
   console.log("New Without Array: ", myArray);
-
-  
-      //console.log("New Without Array: " + newWithoutArray + "\n");
-}
+};
 
 //Help Function
 const assertArraysEqual = function(array1, array2) {
@@ -23,7 +20,7 @@ const assertEqual = function(array1, array2) {
     //console.log("element: " + arrayX, arrayY + "type: " + typeof(arrayX), typeof(arrayY));
     if (array1[i] !== array2[i]) {
       console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    }  
+    }
   }
   console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
 };
@@ -37,5 +34,5 @@ without(testCase1, [1]); // => [2, 3]
 assertArraysEqual(testCase1, [[1, 2, 3]]);
 without(testCase2, [1, 2, "3"]); // => ["1", "2"]
 assertArraysEqual(testCase2, [["1", "2", "3"]]);
-without (testCase3, ["cat", "dog", "bear"]);
+without(testCase3, ["cat", "dog", "bear"]);
 assertArraysEqual(testCase3, [["cat", "dog", "lion"]]);
