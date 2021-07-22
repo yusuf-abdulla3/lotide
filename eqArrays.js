42b655e7530353cefd0dfe0040543324eb954392
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-22 01:10:34 +0000

    Made minor changes

--- eqArrays.js
+++ eqArrays.js
@@ -22,9 +22,9 @@ const eqArrays = function(array1, array2) {
 
 
 //Test Cases
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
