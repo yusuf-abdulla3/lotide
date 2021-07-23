2fb60ba729a7cd4063f68de056bdb6f9f2e6bcdf
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-23 00:59:59 +0000

    Takes items from an array starting from the beginning based on the call function

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+//TEST FUNCTION
+
+const assertArraysEqual = function(array1, array2) {
+  assertEqual(array1, array2);
+};
+
+const assertEqual = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+  for (let i = 0; i < array1.length; i++) {
+    //console.log(array1[i], array2[i]);
+    //console.log("element: " + arrayX, arrayY + "type: " + typeof(arrayX), typeof(arrayY));
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+};
+
+//ACTUAL FUNCTION
+const takeUntil = (arr, callback) => {
+  const results = [];
+  for (let item of arr) {
+    if (!callback(item)) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+  return results;
+};
+
+//TEST CODE
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
+
+
+
+
