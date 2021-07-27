b0b2232ab53efba48b1936c8ed66f81dbe9d4505
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-27 02:15:38 +0000

    Added index.js to the directory and exported all of the functions to it

---
+++ index.js
@@ -0,0 +1,38 @@
+const head = require('./head');
+const middle = require('./middle');
+const tail = require('./tail');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./Findkey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  
+  head: head,
+  middle: middle,
+  tail: tail,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+
+};
